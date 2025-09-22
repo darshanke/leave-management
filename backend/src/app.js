27401881107b6e295378b8d0ebe118065d1c2052
darshan <darshanke646@gmail.com>
2025-09-22 16:55:32 +0530

    enable full cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,12 @@ const allowedOrigins = [
 ];
 app.use(cors({
   origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  credentials: false, 
 }));
+
+app.options('*', cors());
 
 app.use(express.json());
 require("./jobs/autoForwardJob.js");
